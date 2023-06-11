a6fc14d83c3afa2944cd0d72695edb21b6d0efc8
KaminskySergey <kaminsky_s_s@ukr.net>
2023-06-11 19:45:23 +0300

    fonts, theme, start style

---
+++ src/components/Layout/Layout.styled.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+max-width: 1680px;
+margin: 0 auto;
+background-color: ${props => props.theme.background};
+
+`
+
+
+
+export const Section = styled.div`
+  min-height: 100vh;
+  width: 100%;
+  padding: 148px 16px;
+`;
+
+
