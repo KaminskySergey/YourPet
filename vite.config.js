67a95854cc3fac2b64363aa48bc1a3204d6a2053
KaminskySergey <kaminsky_s_s@ukr.net>
2023-06-11 15:36:41 +0300

    start

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "YourPet",
   plugins: [react()],
-  base: '/your-pet/',
-})
+  server: {
+    port: 3000,
+  },
+});
